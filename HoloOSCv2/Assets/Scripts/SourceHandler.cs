b707f9d04d9566928c35d66b2a2f9f8cfc961814
Jindorf <jannik-indorf@hotmail.de>
2019-09-23 15:37:31 +0200

    Source instantiation fixed
    SourceHandler now instantiates the sources evenly around the SourceShell, previously the sources where distributed in line

--- HoloOSCv2/Assets/Scripts/SourceHandler.cs
+++ HoloOSCv2/Assets/Scripts/SourceHandler.cs
@@ -5,30 +5,39 @@ using UnityEngine;
 public class SourceHandler : MonoBehaviour
 {
     public GameObject source;
+    public GameObject shell;
     ArrayList sources = new ArrayList();
+    float sourceRadius;
+    float shellRadius;
 
     [SerializeField]
     private float numberOfObjects = 5;
 
     public void Start() {
-        for(int i=1; i <= numberOfObjects; i++) {
-            AddSourceObject(new Vector3(360/i,0,0));
-        }
+       sourceRadius = source.GetComponent<SphereCollider>().radius;
+       shellRadius = shell.GetComponent<SphereCollider>().radius;
+        InstantiateObjects();
     }
-    public void AddSourceObject(Vector3 pos) {
+
+    //Insantiates numberOfObjects Sources evenly around shell
+    public void InstantiateObjects() {
         //Instantiate Prefab at Runtime
-        
-        GameObject src = Instantiate(source, pos, Quaternion.identity); 
-        src.name = "Source" + sources.Count;
-        src.transform.parent = this.transform;
-        src.GetComponent<SourceObject>().SetID(sources.Count);
-        Vector3 up = pos;
-        Vector3 LookAt = Vector3.Cross(up, -transform.right) + up;
-        src.transform.LookAt(LookAt, up);
-        sources.Add(src);
+        Vector3 spawnPos = shell.transform.position;
+        for (int i = 1; i <= numberOfObjects; i++) {
+            float theta = (2 * Mathf.PI / numberOfObjects) * i;
+            // scale has to be equal in all directions or algorithm has to be changed accordingly
+            spawnPos.x = Mathf.Cos(theta) * shellRadius * shell.transform.localScale.x;
+            spawnPos.z = Mathf.Sin(theta) * shellRadius * shell.transform.localScale.x;
+            GameObject src = Instantiate(source, spawnPos, Quaternion.identity);
+            src.name = "Source" + sources.Count;
+            src.transform.parent = this.transform;
+            src.GetComponent<SourceObject>().SetID(sources.Count);
+            sources.Add(src);
+        }
     }
 
     public void DeleteSourceObject() { 
         //Destroy() the targeted Source 
     }
+
 }
