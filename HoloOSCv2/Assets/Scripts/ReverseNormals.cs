d57458c47de745263c6d708b62d0cd3c992b4bc1
Jindorf <jannik-indorf@hotmail.de>
2019-10-20 08:16:34 +0200

    reverted commit 6a4adf8

---
+++ HoloOSCv2/Assets/Scripts/ReverseNormals.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ReverseNormals : MonoBehaviour
+{
+    
+    void Start()
+    {
+        MeshFilter currentMesh = GetComponent(typeof(MeshFilter)) as MeshFilter;
+        if (currentMesh != null)
+        {
+            //copying current Mesh:
+            Mesh reverseMesh = currentMesh.mesh;
+            Vector3[] normals = reverseMesh.normals;
+            //inverting direction:
+            for (int i = 0; i < normals.Length; i++)
+               normals[i] = -normals[i];
+            reverseMesh.normals = normals;
+
+        }
+    }
+}
